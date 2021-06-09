1ee713c91bb9e2c08566e27a28de875e2bdba012
ZHEXIANG SHENG <37645892+Zhexiang@users.noreply.github.com>
2021-06-08 23:54:23 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# set up config
+git config --global user.name "Zhexiang Sheng"
+git config --global user.email "zhexiang.sheng0930@gmail.com"
+
+# set up environment
+conda env create -f py3.yml
+conda env activate py3
